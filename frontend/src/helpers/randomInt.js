2d6fb0aa5330d790da3a5f37102a1c9af172df91
chrisrappa <rappaproductionsinc@gmail.com>
2025-03-27 15:37:57 -0700

    Revert "basics moved over"
    This reverts commit 66a04e984944924700b25bc937a55311f38e3387.

--- frontend/src/helpers/randomInt.js
+++
@@ -1,7 +0,0 @@
-export default function getRandomInteger(min, max) {
-  // Ensure min and max are integers
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
