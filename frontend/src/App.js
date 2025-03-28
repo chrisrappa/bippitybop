ae5d8cd31f33374ea8bf31fe111c555ef6bf5302
chrisrappa <rappaproductionsinc@gmail.com>
2025-03-27 17:56:04 -0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  Routes, 
+  Route, 
+  Navigate,
+} from 'react-router-dom';
+import { AppContainer } from './styled';
+import Profile from './views/Profile';
+import AppNavigation from './components/AppNavigation';
+import { CssBaseline, useMediaQuery } from '@mui/material';
+import { useState } from 'react';
+import Support from './views/Support';
+import ChatHistoryList from './views/ChatHistoryList';
+import AIChat from './views/AppHome';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+	const drawerWidth = 240;
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+
+  const [drawerOpen, setDrawerOpen] = useState(isMobile ? false : true);
+
+  console.log(
+    '%c Go away, nothing to see here ;)', 
+    'background: #66a18c; color: white; font-size: 20px;'
   );
-}
+  
+  return (
+    <>
+      <AppContainer>
+			  <CssBaseline />
+        <AppNavigation
+          setDrawerOpen={setDrawerOpen} 
+          drawerOpen={drawerOpen} 
+          drawerWidth={drawerWidth}
+        />
+        <Routes>
+
+            <Route path = "/" element={<Navigate to="/ai-chat" />} />
+
+            <Route path = "/support"        element = { <Support         drawerOpen={drawerOpen} drawerWidth={drawerWidth} /> } />
+            <Route path = "/profile/:slug"  element = { <Profile         drawerOpen={drawerOpen} drawerWidth={drawerWidth} /> } />
+            <Route path = "/ai-chat"        element = { <AIChat          drawerOpen={drawerOpen} drawerWidth={drawerWidth} /> } />                        
+            <Route path = "/chat-history"   element = { <ChatHistoryList drawerOpen={drawerOpen} drawerWidth={drawerWidth} /> } />                      
+        </Routes>
+      </AppContainer>
+    </>
+  );
+};
 
 export default App;
